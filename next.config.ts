5f28f43648eef0308474d1502b71bf49a02dffa1
Nikhil034 <nikhilsondarava@lampros.tech>
2025-11-07 10:53:50 +0530

    chore:add redirect on config file to render manifest file.

--- next.config.ts
+++ next.config.ts
@@ -10,15 +10,15 @@ const nextConfig: NextConfig = {
   //     },
   //   ]
   // },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/.well-known/farcaster.json',
-  //       destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019a3e8c-b6bb-25c2-f08b-5d5ade7c2f4b',
-  //       permanent: false, // Use false for a 307 temporary redirect as specified
-  //     },
-  //   ]
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019a3e8c-b6bb-25c2-f08b-5d5ade7c2f4b',
+        permanent: false, // Use false for a 307 temporary redirect as specified
+      },
+    ]
+  },
 };
 
 export default nextConfig;
